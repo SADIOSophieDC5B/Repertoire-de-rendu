d2691693877e06476acfd87284dcca76eedd975d
SADIOSophieDC5B <119399674+SADIOSophieDC5B@users.noreply.github.com>
2023-04-23 16:36:14 +0200

    Exo python scrapping

--- scrap.py
+++ scrap.py
@@ -2,23 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
-# URL de la page à extraire
+
 url = "https://www.scrapethissite.com/pages/simple/"
 
-# Envoie une requête HTTP à l'URL et récupère le contenu de la page
+
 response = requests.get(url)
 content = response.content
 
-# Parse le contenu HTML de la page avec BeautifulSoup
+
 soup = BeautifulSoup(content, "html.parser")
 
-# Trouve toutes les divs avec la classe "country"
+
 countries = soup.find_all("div", class_="country")
 
-# Initialise une liste vide pour stocker les données
+
 data = []
 
-# Pour chaque div "country", récupère les données des classes spécifiées et les ajoute à la liste "data"
+
 for country in countries:
     name = country.find(class_="country-name").get_text()
     info = country.find(class_="country-capital").get_text()
@@ -26,5 +26,5 @@ for country in countries:
     area = country.find(class_="country-area").get_text()
     data.append([name, info, population, area])
 
-# Affiche le tableau de données
+
 print(tabulate(data, headers=["Country Name", "Country Capital", "Population", "Area(Km²)"]))
--- scrap2.py
+++ scrap2.py
@@ -4,21 +4,20 @@ import pandas as pd
 
 results = []
 
-# Loop through the first 10 pages
+
 for i in range(1, 11):
-    # Make a request to the page
+
     response = requests.get(f"https://www.scrapethissite.com/pages/forms/?page_num={i}")
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Find the table and loop through the rows
+  
     table = soup.find('table', {'class': 'table'})
     rows = table.find_all('tr')
 
     for row in rows[1:]:
-        # Find the columns in the row
+      
         cols = row.find_all('td')
 
-        # Extract the data from the columns
         name = cols[0].text.strip()
         year = cols[1].text.strip()
         wins = int(cols[2].text.strip())
@@ -29,16 +28,15 @@ for i in range(1, 11):
         ga = int(cols[7].text.strip())
         diff = int(cols[8].text.strip())
 
-        # Append the data to the results list
         results.append((name, year, wins, losses, ot_losses, pct_wins, gf, ga, diff))
 
-# Create a DataFrame from the scraped data
+
 df = pd.DataFrame(results, columns=['Name', 'Year', 'Wins', 'Losses', 'Ot-losses', '% Wins', 'GF', 'GA', '+/-'])
 
-# Filter the DataFrame based on the +/- column
+
 df_filtered = df[df['+/-'] < 300]
 
-# Write the filtered data to a CSV file
+
 df_filtered.to_csv('results.csv', index=False, header=["Team", "Year", "Wins", "Losses", "OT Losses", "% Wins", "GF", "GA", "+/-"], sep=',')
 
 print(df_filtered)
